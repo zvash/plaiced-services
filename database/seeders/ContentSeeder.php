8cd066dafc8ea19a105f98d3946c41ae077de185
Ali Shafiee <alshf89@gmail.com>
2021-08-24 09:47:28 +0430

    Content Seeder

---
+++ database/seeders/ContentSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Content;
+use Database\Seeders\Traits\HasSeeder;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+
+class ContentSeeder extends Seeder
+{
+    use HasSeeder;
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach ($this->seeds() as $content) {
+            Content::create($this->resolveAttributes($content));
+        }
+    }
+
+    /**
+     * Resolve array attributes.
+     *
+     * @param  array  $content
+     * @return array
+     */
+    private function resolveAttributes(array $content)
+    {
+        $faker = Factory::create();
+
+        return array_merge($content, [
+            'featured' => $faker->boolean(),
+            'synopsis' => $faker->paragraph(),
+            'viewership_metrics' => $faker->paragraph(),
+            'general_comment' => $faker->paragraph(),
+            'shipping_contact_name' => $faker->name(),
+            'shipping_contact_telephone' => $faker->e164PhoneNumber(),
+            'address' => $faker->address(),
+            'city' => $faker->city(),
+            'state' => $faker->state(),
+            'postal_code' => $faker->postcode(),
+            'locations' => $faker->words(3),
+            'demographic_age' => $faker->words(3),
+            'demographic_gender' => $faker->words(3),
+            'demographic_geography_id' => $faker->words(3),
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             AdvertiserSeeder::class,
             ContentCreatorSeeder::class,
+            ContentSeeder::class,
         ]);
     }
 
